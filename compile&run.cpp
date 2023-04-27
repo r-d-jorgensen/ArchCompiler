6c9173fd04b588911413d652406358ccfac3bdc9
Ckossler <131414970+Ckossler@users.noreply.github.com>
2023-04-27 08:32:54 -0500

    Update compile&run.cpp
    Added some basic code for the execution piece. Updated executeInstruction, printCurrentState, and runMachineCode functions, and added instructionCount function.
    
    NOTE: executeInstruction is NOT close to completed - only a few of the instructions actually have code. The rest have a "replace me" print to console because I did not program them.
    
    Also, I'm not sure the best way to handle program inputs.... it would probably interrupt the table as is, and it wouldn't be the easiest thing to job off inputs to the printFunction since the input also sets the register value. That said, it's probably not SUPER important since I don't think the Fibonacci code requires inputs, but I can't remember

--- compile&run.cpp
+++ compile&run.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string>
+#include <iostream>
 using namespace std;
 
 void compileMachineCode() {
@@ -14,26 +15,100 @@ void compileMachineCode() {
     return;
 }
 
-int registars[10] = {0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0, 0};
+int registers[10] = {0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0, 0}; //registers are assumed to be restricted to integer values; represented in decimal
+int instructionLength = 4; //instruction length of 4 bits will be used to decode the instructions
+int* ptr=registers; //pointer for executing instructions; initialized to memory location of registers[0]
 
-void executeInstruction(string instruction) {
-    // excecute the instruction
+int executeInstruction(string instruction) { //the function will return any outputs rather than printing them directly. printInstruction will handle printing the output
+    if(instruction=="0000"){ //move pointer up
+		ptr++;
+	}else if(instruction=="0001"){ //move pointer down
+		ptr--;
+	}else if(instruction=="0010"){ //increment location by 1
+		(*ptr)++;
+	}else if(instruction=="0011"){ //decrement location by 1
+		(*ptr)--;
+	}else if(instruction=="0100"){ //add pointer value to location value
+		cout<<"Replace me!"<<endl;
+	}else if(instruction=="0101"){ //subtract pointer value to location value
+		cout<<"Replace me!"<<endl;
+	}else if(instruction=="0110"){ //output (print) value at pointer
+		return *ptr;
+	}else if(instruction=="0111"){ //input and store value to location at pointer
+		cout<<"Replace me!"<<endl;
+	}else if(instruction=="1000"){ //store location value in pointer
+		cout<<"Replace me!"<<endl;
+	}else if(instruction=="1001"){ //load pointer value to location
+		cout<<"Replace me!"<<endl;
+	}else if(instruction=="1010"){ //OR pointer value and location value store in pointer value
+		cout<<"Replace me!"<<endl;
+	}else if(instruction=="1011"){ //AND pointer value and location value store in pointer value
+		cout<<"Replace me!"<<endl;
+	}else if(instruction=="1100"){ //== equality check between pointer and location values
+		cout<<"Replace me!"<<endl;
+	}else if(instruction=="1101"){ //if pointer value is 0 go to end of loop
+		cout<<"Replace me!"<<endl;
+	}else if(instruction=="1110"){ //end of loop
+		cout<<"Replace me!"<<endl;
+	}else if(instruction=="1111"){ //go to end loop
+		cout<<"Replace me!"<<endl;
+	}else{
+		cout<<"The instruction was not valid binary"<<endl;
+	}
+
+    return -9999; //this means there is no output
+}
+
+void printCurrentState(string instruction,int ct,int output) {
+    // print the instruction being executed
+    // print the values of each register
+	cout<<ct<<"			"<<instruction<<"		"<<registers[0]<<"	"<<registers[1]<<"	"<<registers[2]<<"	"<<registers[3]<<"	"<<registers[4]<<"	"<<registers[5]<<"	"<<registers[6]<<"	"<<registers[7]<<"	"<<registers[8]<<"	"<<registers[9];
+	if(output!=-9999){
+		cout<<"	"<<output<<endl;
+	}else cout<<"	"<<endl;
+
     return;
 }
 
-void printCurrentState(string instruction) {
-    // print the instruction being executed
-    // print the values of each registar
-    return;
+int instructionCount(std::string machineCode,int instructionCodeLength){
+	if(machineCode.length()%instructionCodeLength!=0){
+		return -1; //error checking if we receive machine code of invalid length
+	} else {
+		return machineCode.length()/instructionCodeLength;
+	}
 }
 
 void runMachineCode() {
     // read code from machineCode.txt
     string instructions = "00100010001001100010"; // or array of instructions [0000, 0000, ....]
+    
+    //get the total instruction count, for looping purposes and error checking
+	int instCnt = instructionCount(instructions,instructionLength);
+	if(instCnt==-1){
+		cout << "The machine code is invalid length, silly!" << endl; //print error and return early
+		return;
+	}
+    
+    int programCounter = 0;
+	int output=-9999;
+    
+    // set up output table
+	cout<<"Instruction Count	Instruction	R0	R1	R2	R3	R4	R5	R6	R7	R8	R9	Program Output" <<endl;
+	cout<<"---------------------------------------------------------------------------------------------------------------------------------------" <<endl;
+	cout<<"n/a			n/a		"<<registers[0]<<"	"<<registers[1]<<"	"<<registers[2]<<"	"<<registers[3]<<"	"<<registers[4]<<"	"<<registers[5]<<"	"<<registers[6]<<"	"<<registers[7]<<"	"<<registers[8]<<"	"<<registers[9]<<endl;
+    
+	// loop over each instruction
+	for(int i = 0; i < instCnt; i++){
+		//grab current instruction
+		string givenInstruction(instructions,programCounter,instructionLength);
 
-    // loop over each instruction
-        executeInstruction("0010");
-        printCurrentState("0010");
+		//take action based on instruction
+		output=executeInstruction(givenInstruction);
+		printCurrentState(givenInstruction,i,output);
+
+		//move counter up to grab next instruction
+		programCounter = programCounter + 4;
+	}
 
     return;
 }
